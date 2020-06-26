60ab4c67a550a0071c6962a74dbc1fe14e19d328
knzgage <40836138+knzgage@users.noreply.github.com>
2020-06-26 14:58:32 -0700

    Part 8: Can now pick up health potions with 'g'

--- engine.py
+++ engine.py
@@ -5,10 +5,11 @@ and I followed along with this tutorial through reddit.com in the subreddit r/ro
 import tcod as libtcod
 
 from components.fighter import Fighter
+from components.inventory import Inventory
 from death_functions import kill_monster, kill_player
 from entity import Entity, get_blocking_entities_at_location
 from fov_functions import initialize_fov, recompute_fov
-from game_messages import MessageLog
+from game_messages import Message, MessageLog
 from game_states import GameStates
 from input_handlers import handle_keys
 from map_objects.game_map import GameMap
@@ -50,8 +51,10 @@ def main():
     }
 
     fighter_component = Fighter(hp=30, defense=2, power=5)
+    inventory_component = Inventory(26)
     player = Entity(0, 0, '@', libtcod.white, "Player",
-                    blocks=True, render_order=RenderOrder.ACTOR, fighter=fighter_component)
+                    blocks=True, render_order=RenderOrder.ACTOR,
+                    fighter=fighter_component, inventory=inventory_component)
     entities = [player]
 
     libtcod.console_set_custom_font("arial10x10.png", libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
@@ -93,6 +96,7 @@ def main():
         action = handle_keys(key)
 
         move = action.get("move")
+        pickup = action.get("pickup")
         exit = action.get("exit")
         fullscreen = action.get("fullscreen")
 
@@ -116,6 +120,16 @@ def main():
 
                 game_state = GameStates.ENEMY_TURN
 
+        elif pickup and game_state == GameStates.PLAYERS_TURN:
+            for entity in entities:
+                if entity.item and entity.x == player.x and entity.y == player.y:
+                    pickup_results = player.inventory.add_item(entity)
+                    player_turn_results.extend(pickup_results)
+
+                    break
+            else:
+                message_log.add_message(Message("There is nothing here to pick up.", libtcod.yellow))
+
         if exit:
             return True
 
@@ -125,6 +139,7 @@ def main():
         for player_turn_result in player_turn_results:
             message = player_turn_result.get("message")
             dead_entity = player_turn_result.get("dead")
+            item_added = player_turn_result.get("item_added")
 
             if message:
                 message_log.add_message(message)
@@ -136,6 +151,11 @@ def main():
                     message = kill_monster(dead_entity)
 
                 message_log.add_message(message)
+
+            if item_added:
+                entities.remove(item_added)
+
+                game_state = GameStates.ENEMY_TURN
 
         if game_state == GameStates.ENEMY_TURN:
             for entity in entities:
