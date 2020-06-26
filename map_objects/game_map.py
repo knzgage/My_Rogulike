72b2f881f37d3a43c648f2be600387b738203f0b
knzgage <40836138+knzgage@users.noreply.github.com>
2020-06-26 10:23:18 -0700

    Part 8: Health potions now appear around the map

--- map_objects/game_map.py
+++ map_objects/game_map.py
@@ -21,7 +21,7 @@ class GameMap:
         return tiles
 
     def make_map(self, max_rooms, room_min_size, room_max_size, map_width, map_height, player, entities,
-                 max_monsters_per_room):
+                 max_monsters_per_room, max_items_per_room):
         # Create two rooms
         rooms = []
         num_rooms = 0
@@ -68,8 +68,8 @@ class GameMap:
                         self.create_v_tunnel(prev_y, new_y, prev_x)
                         self.create_h_tunnel(prev_x, new_x, new_y)
 
-                #Add monsters to the room
-                self.place_entities(new_room, entities, max_monsters_per_room)
+                # Add monsters to the room
+                self.place_entities(new_room, entities, max_monsters_per_room, max_items_per_room)
                 # Append the new room to the list
                 rooms.append(new_room)
                 num_rooms += 1
@@ -91,9 +91,10 @@ class GameMap:
             self.tiles[x][y].blocked = False
             self.tiles[x][y].block_sight = False
 
-    def place_entities(self, room, entities, max_monsters_per_room):
-        # Get a random number of monsters
+    def place_entities(self, room, entities, max_monsters_per_room, max_items_per_room):
+        # Get a random number of monsters and items
         number_of_monsters = randint(0, max_monsters_per_room)
+        number_of_items = randint(0, max_items_per_room)
 
         for i in range(number_of_monsters):
             # Choose a random location in the room
@@ -112,9 +113,19 @@ class GameMap:
                     fighter_component = Fighter(hp=16, defense=1, power=4)
                     ai_component = BasicMonster()
                     monster = Entity(x, y, 'T', libtcod.darker_green, "Troll", blocks=True,
-                                     render_order=RenderOrder.ACTOR, fighter=fighter_component, ai=ai_component )
+                                     render_order=RenderOrder.ACTOR, fighter=fighter_component, ai=ai_component)
 
                 entities.append(monster)
+
+        for i in range(number_of_items):
+            # Choose a random location in the room
+            x = randint(room.x1 + 1, room.x2 - 1)
+            y = randint(room.y1 + 1, room.y2 - 1)
+
+            if not any([entity for entity in entities if entity.x == x and entity.y == y]):
+                item = Entity(x, y, '!', libtcod.violet, 'Healing Potion', render_order=RenderOrder.ITEM)
+
+                entities.append(item)
 
     # Tells whether a  tile blocks movement or not
     def is_blocked(self, x, y):
