7ebe6cde4a95caf32f4c69cae71c61c4ebb61392
knzgage <40836138+knzgage@users.noreply.github.com>
2020-06-30 11:13:56 -0700

    Part 8: Displays inventory with items when pressing 'i' (cannot use items yet)

--- engine.py
+++ engine.py
@@ -78,6 +78,7 @@ def main():
     mouse = libtcod.Mouse()
 
     game_state = GameStates.PLAYERS_TURN
+    previous_game_state = game_state
 
     # Game loop goes until esc is pressed
     while not libtcod.console_is_window_closed():
@@ -87,7 +88,7 @@ def main():
             recompute_fov(fov_map, player.x, player.y, fov_radius, fov_light_walls, fov_algorithm)
 
         render_all(con, panel, entities, player, game_map, fov_map, fov_recompute, message_log, screen_width,
-                   screen_height, bar_width, panel_height, panel_y, mouse, colors)
+                   screen_height, bar_width, panel_height, panel_y, mouse, colors, game_state)
         fov_recompute = False
         libtcod.console_flush()
 
@@ -97,6 +98,7 @@ def main():
 
         move = action.get("move")
         pickup = action.get("pickup")
+        show_inventory = action.get("show_inventory")
         exit = action.get("exit")
         fullscreen = action.get("fullscreen")
 
@@ -130,8 +132,15 @@ def main():
             else:
                 message_log.add_message(Message("There is nothing here to pick up.", libtcod.yellow))
 
+        if show_inventory:
+            previous_game_state = game_state
+            game_state = GameStates.SHOW_INVENTORY
+
         if exit:
-            return True
+            if game_state == GameStates.SHOW_INVENTORY:
+                game_state = previous_game_state
+            else:
+                return True
 
         if fullscreen:
             libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
